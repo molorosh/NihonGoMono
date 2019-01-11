8a4c2bc9c86725d46aa08d75bac304fc237d75a3
unknown <molorosh@gmail.com>
2019-01-11 23:31:51 +0000

    added a little bit of javascript

--- docs/js/nihongomono.js
+++ docs/js/nihongomono.js
@@ -1,1 +1,8 @@
-console.log("nihon go mono");
+
+
+function nannichi(code){
+	var diag_str = "nannichi('" + code + "')";
+	console.log(diag_str);
+	var para = document.getElementById("id_p_date");
+	para.innerHTML = diag_str;
+}
