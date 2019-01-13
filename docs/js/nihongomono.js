c56b550083b33a2ce38ce88b9a56037c654dcc34
unknown <molorosh@gmail.com>
2019-01-13 12:05:09 +0000

    added resizing settings for japanese date

--- docs/js/nihongomono.js
+++ docs/js/nihongomono.js
@@ -37,10 +37,24 @@ function nannichi(code){
 	let para = document.getElementById("id_p_date");
 	para.innerHTML = the_date.formattedHtml;
 	// 
-	let english_html = "<button class='nannichi_button' onclick='nannichi_show_english();'><ruby>英<rp>(</rp><rt>えい</rt><rp>)</rp></ruby></button>";
+	let english_html = "<button class='nannichi_button' onclick='nannichi_show_english();'><ruby>英<rp>(</rp><rt>えい</rt><rp>)</rp>語<rp>(</rp><rt>ご</rt><rp>(</rp></ruby></button>";
 	english_html += " <span id='id_span_date_english'>";
 	english_html += the_date.englishStr;
 	english_html += "</span>";
 	let para_english = document.getElementById("id_p_date_english");
 	para_english.innerHTML = english_html;
+}
+
+function resizeContent(the_size, the_element){
+	console.log(the_size);
+	let resizables = document.getElementsByClassName("resizable");
+	console.log(resizables.length);
+	for(let x = 0, xMax = resizables.length; x < xMax; x++){
+		resizables[x].style.fontSize = the_size;
+	}
+	let resizers = document.getElementsByClassName("fontSizer");
+	for(let x = 0, xMax = resizers.length; x < xMax; x++){
+		resizers[x].classList.remove('fontSizerSelected');
+	}
+	the_element.classList.add('fontSizerSelected');
 }
