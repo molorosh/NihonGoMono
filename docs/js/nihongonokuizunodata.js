74ff50e4afb601d2641c525a93575bbe294e7a49
Howard Ricketts <molorosh@gmail.com>
2019-01-14 20:11:02 +0000

    added new Quiz page

---
+++ docs/js/nihongonokuizunodata.js
@@ -0,0 +1,13 @@
+var kuizo_titles = [
+	{ val: 'colors_lvl_001', category: 'Colours', level: 1 },
+	{ val: 'calendar_lvl_001', category: 'Calendar', level: 1 }
+];
+
+var kuizo_data = [
+	{ 
+		key: 'colors_lvl_001'
+	},
+	{ 
+		key: 'calendar_lvl_001' 
+	}
+]
