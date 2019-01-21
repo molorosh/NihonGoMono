b66b969fa0a325cd1ed32dcf000cdbebbbf84590
unknown <molorosh@gmail.com>
2019-01-21 23:45:18 +0000

    added an autoresize to drag and drop window

--- docs/js/nihongonokuizu.js
+++ docs/js/nihongonokuizu.js
@@ -1,6 +1,17 @@
 let kuizo_is_debug = false;
 let kuizo_selected = null;
 
+function kuizoResize(){
+	var screen_height = $(window).height();
+	var content_div_ver_pos = $("#id_div_quizContent")
+	var pos = content_div_ver_pos.position();
+	var pos_top = pos.top;
+	var new_height = screen_height - (pos_top + 25);
+	//console.log("screen_height = " + screen_height);
+	//console.log("pos_top = " + pos_top);
+	//console.log("new_height = " + new_height);
+	content_div_ver_pos.height(new_height);
+}
 
 function kuizoIsCorrect(idQuestion, idAnswer){
 	//console.log(idQuestion + " : " + idAnswer);
@@ -65,15 +76,7 @@ function kuizuSelectSetup(){
 	for(var x = 0, xMax = kuizo_titles.length; x < xMax; x++){
 		$('#id_select_quizTheme').append( $('<option>', { value: kuizo_titles[x].val, text: (kuizo_titles[x].category + " level " + ("00" + kuizo_titles[x].level).slice(-2) ) }) );
 	}
-	var screen_height = $(window).height();
-	var content_div_ver_pos = $("#id_div_quizContent")
-	var pos = content_div_ver_pos.position();
-	var pos_top = pos.top;
-	var new_height = screen_height - (pos_top + 25);
-	//console.log("screen_height = " + screen_height);
-	//console.log("pos_top = " + pos_top);
-	//console.log("new_height = " + new_height);
-	content_div_ver_pos.height(new_height);
+	kuizoResize();
 }
 
 function getIndexArray(size){
