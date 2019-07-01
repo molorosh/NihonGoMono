8f98ae45aae2dcd275ad10d4069aa084625d16e2
Howard Ricketts <molorosh@gmail.com>
2019-07-01 23:46:44 +0100

    added some adjective sets to the quiz mode

--- docs/js/nihongonokuizunodata.js
+++ docs/js/nihongonokuizunodata.js
@@ -3,7 +3,12 @@ var kuizo_titles = [
 	{ val: 'calendar_lvl_001', category: 'Calendar', level: 1 },
 	{ val: 'calendar_lvl_002', category: 'Calendar', level: 2 },
 	{ val: 'calendar_lvl_003', category: 'Calendar', level: 3 },
-];
+	{ val: 'adjective_001_food', category: 'Adjective', level: 1 },
+	{ val: 'adjective_002_travel', category: 'Adjective', level: 2 },
+    { val: 'adjective_003_places', category: 'Adjective', level: 3 },	
+	{ val: 'adjective_004_preference', category: 'Adjective', level: 4 },	
+	{ val: 'adjective_005_building', category: 'Adjective', level: 5 }
+	];
 
 var kuizo_data = [
 	{ 
@@ -21,7 +26,7 @@ var kuizo_data = [
 			
 			{ text: 'しろ', answers: ['White'], type: 'hiragana' },
 			{ text: '白', answers: ['White'], type: 'kanji' },
-			
+		
 			{ text: 'あか', answers: ['Red'], type: 'hiragana' },
 			{ text: '赤', answers: ['Red'], type: 'kanji' },
 			
@@ -54,6 +59,95 @@ var kuizo_data = [
 			{ text: 'とし', answers: ['Year'], type: 'hiragana' },
 			{ text: 'ねん', answers: ['Year'], type: 'hiragana' },
 			{ text: '年', answers: ['Year'], type: 'kanji' }
+		]
+	},
+	{ 
+		key: 'adjective_001_food',
+		answers: [
+			{ text: 'Delicious' , type: 'answer' },
+			{ text: 'Horrible' , type: 'answer' },
+			{ text: 'Fast' , type: 'answer' },
+			{ text: 'Slow' , type: 'answer' },
+			{ text: 'Expensive' , type: 'answer' },
+			{ text: 'Cheap' , type: 'answer' }
+		],
+		phrases: [
+			{ text: 'おいしい ', answers: ['Delicious'], type: 'hiragana' },
+			{ text: 'まずい', answers: ['Horrible'], type: 'hiragana' },
+			{ text: 'はやい', answers: ['Fast'], type: 'hiragana' },
+			{ text: 'おそい', answers: ['Slow'], type: 'hiragana' },
+			{ text: 'たかい', answers: ['Expensive'], type: 'hiragana' },
+			{ text: 'やすい', answers: ['Cheap'], type: 'hiragana' },
+		]
+	},
+	{ 
+		key: 'adjective_002_travel',
+		answers: [
+			{ text: 'Inconvenient' , type: 'answer' },
+			{ text: 'Convenient' , type: 'answer' },
+			{ text: 'Distant/Far' , type: 'answer' },
+			{ text: 'Close/Near' , type: 'answer' },
+			{ text: 'Comfortable/Easy' , type: 'answer' }
+		],
+		phrases: [
+			{ text: 'ふべん.な', answers: ['Inconvenient'], type: 'hiragana' },
+			{ text: 'べんり.な', answers: ['Convenient'], type: 'hiragana' },
+			{ text: 'とおい', answers: ['Distant/Far'], type: 'hiragana' },
+			{ text: 'ちかい', answers: ['Close/Near'], type: 'hiragana' },
+			{ text: 'きらく.な', answers: ['Comfortable/Easy'], type: 'hiragana' },
+		]
+	},
+	{ 
+		key: 'adjective_003_places',
+		answers: [
+			{ text: 'Famous' , type: 'answer' },
+			{ text: 'Ordinary' , type: 'answer' },
+			{ text: 'Crowded/Busy' , type: 'answer' },
+			{ text: 'Quiet/Empty' , type: 'answer' }
+		],
+		phrases: [
+			{ text: 'ゆうめい.な', answers: ['Famous'], type: 'hiragana' },
+			{ text: 'へいぼん.な', answers: ['Ordinary'], type: 'hiragana' },
+			{ text: 'にぎやか.な', answers: ['Crowded/Busy'], type: 'hiragana' },
+			{ text: 'しずか.な', answers: ['Quiet/Empty'], type: 'hiragana' }
+		]
+	},
+	{ 
+		key: 'adjective_004_preference',
+		answers: [
+			{ text: 'Clean/Pretty' , type: 'answer' },
+			{ text: 'Cute' , type: 'answer' },
+			{ text: 'Cool/Stylish' , type: 'answer' },
+			{ text: 'Good' , type: 'answer' }
+		],
+		phrases: [
+			{ text: 'きれい', answers: ['Clean/Pretty'], type: 'hiragana' },
+			{ text: 'かわいい', answers: ['Cute'], type: 'hiragana' },
+			{ text: 'かっこう', answers: ['Cool/Stylish'], type: 'hiragana' },
+			{ text: 'よい', answers: ['Good'], type: 'hiragana' }
+		]
+	},
+	{ 
+		key: 'adjective_005_building',
+		answers: [
+			{ text: 'Big' , type: 'answer' },
+			{ text: 'Small' , type: 'answer' },
+			{ text: 'Clean/New' , type: 'answer' },
+			{ text: 'Old' , type: 'answer' },
+			{ text: 'Sunny/Bright' , type: 'answer' },
+			{ text: 'Shady/Dark' , type: 'answer' },
+			{ text: 'Spacious' , type: 'answer' },
+			{ text: 'Cramped' , type: 'answer' }
+		],
+		phrases: [
+			{ text: 'おおきい', answers: ['Big'], type: 'hiragana' },
+			{ text: 'ちいさい', answers: ['Small'], type: 'hiragana' },
+			{ text: 'あたらしい', answers: ['Clean/New'], type: 'hiragana' },
+			{ text: 'ふるい', answers: ['Old'], type: 'hiragana' },
+			{ text: 'あかるい', answers: ['Sunny/Bright'], type: 'hiragana' },
+			{ text: 'くらい', answers: ['Shady/Dark'], type: 'hiragana' },
+			{ text: 'ひろい', answers: ['Spacious'], type: 'hiragana' },
+			{ text: 'せまい ', answers: ['Cramped'], type: 'hiragana' }
 		]
 	},
 	{ 
